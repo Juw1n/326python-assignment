6fe64d93c92ca3b805e1851bd0d946c7d14b5010
Juw1n <juwonolaitan68@gmail.com>
2025-05-18 23:19:33 +0100

    Create caesar cipher.py

---
+++ caesar cipher.py
@@ -0,0 +1,20 @@
+// program to implement a ceaser cipher
+#include <stdio.h>            
+    text = input("Enter text to encrypt: ")
+    shift = int(input("Enter shift value: "))
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+
+for char in message:
+    if char.lower() in alphabet:
+        is_upper = char.isupper()
+        index = alphabet.index(char.lower())
+        new_index = (index + shift) % 26
+        new_char = alphabet[new_index]
+        if is_upper:
+            new_char = new_char.upper()
+        result += new_char
+    else:
+        result += char 
+
+print("Encrypted message:", result)
